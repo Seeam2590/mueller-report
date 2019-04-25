8b15d859e20753b094bbba6fba3817642c9f7e35
Seeam2590 <seeamshahid@gmail.com>
2019-04-25 14:23:51 -0400

    Created a word cloud app

--- process_report.R
+++ process_report.R
@@ -1,8 +1,11 @@
 library(tidyverse)
 library(pdftools)
+library(shiny)
 
 # Download report from https://www.documentcloud.org/documents/5955210-Redacted-Mueller-Report.html
 mueller_report_txt <- pdf_text("Redacted-Mueller-Report.pdf")
+
+write_rds(mueller_report_txt, path = "mueller_report/data.rds")
 
 mueller_report <- tibble(
   page = 1:length(mueller_report_txt),
@@ -22,3 +25,7 @@ mueller_report <-
   )
 
 write_csv(mueller_report, "mueller_report.csv")
+
+mueller_report <- read_csv("mueller_report.csv")
+
+create_wordcloud(mueller_report_txt, num_words = 100, background = "white")
